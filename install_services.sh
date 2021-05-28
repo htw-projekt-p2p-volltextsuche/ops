8e41f7f5b174db50e7f5e0520dba44b513015054
zink-l <leon.enzenberger@protonmail.com>
2021-05-28 22:13:20 +0200

    Add explanations to scripts

--- install_services.sh
+++ install_services.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e;
+
+# Update 
 sudo apt update;
 sudo apt full-upgrade;
+
+# Add necessery programs
 sudo apt install nano;
 sudo apt install ufw;
 sudo apt install snapd;
---
+++ setup_firewall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e;
+
+# Standard config for http Server
+sudo ufw default deny incoming;
+sudo ufw default allow outgoing;
+sudo ufw allow ssh;
+sudo ufw allow http;
+
+# Necessery ports for kubernetes
+sudo ufw allow 16443
+sudo ufw allow 10250
+sudo ufw allow 10255
+sudo ufw allow 25000
+sudo ufw allow 12379
+sudo ufw allow 10257
+sudo ufw allow 10259
+sudo ufw allow 19001
+
+sudo ufw enable;
