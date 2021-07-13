277f59b403ac95cd09a442e18d0c3647c068b23f
zink-l <leon.enzenberger@protonmail.com>
2021-07-13 16:56:47 +0200

    Unify all scripts in install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e;
+
+sh setup/install_apps.sh;
+sh setup/setup_firewall.sh;
+sh setup/setup_routing.sh;
+sh setup/install_docker.sh;
+sh setup/configure_docker_rootless.sh;
+sh setup/install_docker-compose.sh;
+
+echo "ALL SCRIPTS RAN WITHOUT ISSUES - SERVER IS SET UP!";
--- setup_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo systemctl disable --now docker.service docker.socket;
-/usr/bin/dockerd-rootless-setuptool.sh install;
-export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock;
