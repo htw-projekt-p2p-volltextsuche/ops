e111fc68eec7fc646d9290b0dbe1c7cff200e028
zink-l <leon.enzenberger@protonmail.com>
2021-07-13 16:25:54 +0200

    Add setup_firewall script

---
+++ install/setup_firewall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e;
+
+sudo ufw default deny incoming;
+sudo ufw default allow outgoing;
+
+sudo ufw allow ssh;
+sudo ufw allow http;
+
+sudo ufw allow 8080/tcp;
+sudo ufw allow 8421/tcp;
+sudo ufw allow 8090;
+sudo ufw allow 8430;
+sudo ufw allow 8081;
+
+sudo ufw enable;
