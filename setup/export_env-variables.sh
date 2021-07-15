12f95ff5725b2a2f71fa4f83095af17134d23b82
zink-l <leon.enzenberger@protonmail.com>
2021-07-15 14:53:53 +0200

    Replace source in eport by dot

--- setup/export_env-variables.sh
+++ setup/export_env-variables.sh
@@ -4,4 +4,4 @@ echo "DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock" >> ~/.bashrc;
 echo "PATH=/usr/bin:$PATH" >> ~/.bashrc;
 echo "DOCKER_HOST=unix:///run/user/1000/docker.sock" >> ~/.bashrc;
 
-source ~/.bashrc;
+. ~/.bashrc
